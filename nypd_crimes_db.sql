94cb7011771d544ec6341f897d7804c5621e9a75
Manuel Mejia <bxprdr25@hotmail.com>
2021-02-18 11:14:01 -0500

    Finalized the Jupyter notebook with SQLAlchemy join and commenting each section

---
+++ nypd_crimes_db.sql
@@ -0,0 +1,12 @@
+-- Database: nypd_crimes
+
+-- DROP DATABASE nypd_crimes;
+
+CREATE DATABASE nypd_crimes
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
